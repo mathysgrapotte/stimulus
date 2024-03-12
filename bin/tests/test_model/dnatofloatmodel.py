8a4ad8eb4f0588a59c884cb98045cfe80174d04e
mgrapotte <mathysgrapotte@gmail.com>
2024-03-12 15:28:33 +0100

    it seems that the user will not have to use dict, simply provide the input of the model in the right order in the forward function

--- bin/tests/test_model/dnatofloatmodel.py
+++ bin/tests/test_model/dnatofloatmodel.py
@@ -3,15 +3,14 @@ import torch
 ## this is a simple model that takes as input a 1D tensor of any size, apply some convolutional layer and outputs a single value using a maxpooling layer and a softmax function.
 
 class SimpleModel(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, kernel_size: int = 3, pool_size: int = 2):
         super(SimpleModel, self).__init__()
-        self.conv1 = torch.nn.Conv1d(1, 1, 3)
-        self.pool = torch.nn.MaxPool1d(2, 2)
+        self.conv1 = torch.nn.Conv1d(1, 1, kernel_size)
+        self.pool = torch.nn.MaxPool1d(pool_size, pool_size)
         self.fc1 = torch.nn.Linear(1, 1)
         self.softmax = torch.nn.Softmax(dim=1)
     
     def forward(self, x):
-        x = x['dna']
         x = self.conv1(x)
         x = self.pool(x)
         x = self.fc1(x)
