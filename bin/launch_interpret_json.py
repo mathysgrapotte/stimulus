c635484453cdf37bc4525fddf839c5c72b597d17
alessiovignoli <alessio.vignoli3@studio.unibo.it>
2024-04-08 16:32:43 +0200

    allowing noise and split to be not present and adding experiment name to custom dictionaries

--- bin/launch_interpret_json.py
+++ bin/launch_interpret_json.py
@@ -25,8 +25,6 @@ def get_args():
 
 def interpret_json(input_json: dict) -> list:
 
-    # TODO handle no noise or splitter
-
     # Initialize json schema it checks for correctness of the Json architecture and fields / values
     schema = JsonSchema(input_json)
 
@@ -42,23 +40,29 @@ def interpret_json(input_json: dict) -> list:
     # combine split possibilities with noise ones in a all vs all manner, each splitter wil be assigned to each noiser
     list_of_json_to_write = []
 
-    # Check if both lists are empty
-    if not list_noise_combinations and not list_split_combinations:
-        list_of_json_to_write.append({"experiment": schema.experiment})
-    else:
-        if not list_split_combinations:  # Check if list_split_combinations is empty
-            for noiser_dict in list_noise_combinations:
-                list_of_json_to_write.append({"experiment": schema.experiment, "noise": noiser_dict})
-        else:
-            for splitter_dict in list_split_combinations:
-                if not list_noise_combinations:  # Check if list_noise_combinations is empty
-                    list_of_json_to_write.append({"experiment": schema.experiment, "split": splitter_dict})
-                else:
-                    list_of_json_to_write.append({"experiment": schema.experiment, "noise": noiser_dict, "split": splitter_dict})
+    # The  pipeline has always to happen at least once, aka on the data itself untouched. this sould always be passed through the pipeline. This line is creating the json for that.
+    list_of_json_to_write.append({"experiment": schema.experiment, "noise": None, "split": None})
 
-    # deal wiht custom if present, in this case nothing at all will be done to the dictionary, it will just be passed as it is
+    # if only noise is present, we do not care for the presence of custom in this case bacause it is handled by itself later on
+    if list_noise_combinations and not list_split_combinations:
+        for noiser_dict in list_noise_combinations:
+            list_of_json_to_write.append({"experiment": schema.experiment, "noise": noiser_dict, "split": None})
+
+    # if only split is present, again custom is not influential
+    elif list_split_combinations and not list_noise_combinations:
+        for splitter_dict in list_split_combinations:
+            list_of_json_to_write.append({"experiment": schema.experiment, "noise": None, "split": splitter_dict})
+    
+    # when both noise and split flag are present
+    else:
+        for noiser_dict in list_noise_combinations:
+            for splitter_dict in list_split_combinations:
+                list_of_json_to_write.append({"experiment": schema.experiment, "noise": noiser_dict, "split": splitter_dict})
+
+    # deal wiht custom if present, in this case nothing at all will be done to the dictionaries present in the list except adding the experiment name to it. The user is responsible for the dict inside custom to be correct and ready for the csv_launcher
     for custom_dict in schema.custom_arg :
-        list_of_json_to_write.append(custom_dict)
+        new_dict = {**{"experiment": schema.experiment}, **custom_dict}
+        list_of_json_to_write.append(new_dict)
 
     return list_of_json_to_write
 
@@ -79,11 +83,11 @@ def main(config_json: str, out_dir_path: str) -> str:
     os.makedirs(out_dir_path, exist_ok=True)
 
     # Populate the directory with files containing the single SJon combination
-    for i, elements in enumerate(list_json):
+    for i, interpreted_json in enumerate(list_json):
         suffix = os.path.splitext(os.path.basename(config_json))[0]
         file_path = os.path.join(out_dir_path, f"{suffix}-#{i+1}.json")
-        with open(file_path, 'w') as file:
-            file.write(f"{elements}\n")
+        with open(file_path, 'w') as json_file:
+            json.dump(interpreted_json, json_file)
 
 
 if __name__ == "__main__":
