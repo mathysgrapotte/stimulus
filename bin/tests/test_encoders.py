a9234201fbaf79876f87e19e7da5a484c6389849
mgrapotte <mathysgrapotte@gmail.com>
2024-04-29 15:52:57 +0200

    [test] adding tests for changing strings into categorical variables

--- bin/tests/test_encoders.py
+++ bin/tests/test_encoders.py
@@ -31,7 +31,7 @@ class TestMyClass(unittest.TestCase):
 import numpy as np
 import numpy.testing as npt
 import unittest
-from bin.src.data.encoding.encoders import TextOneHotEncoder, IntRankEncoder
+from bin.src.data.encoding.encoders import TextOneHotEncoder, IntRankEncoder, StrClassificationIntEncoder
 
 
 class TestTextOneHotEncoderDna(unittest.TestCase):
@@ -111,8 +111,18 @@ class TestIntRankEncoder(unittest.TestCase):
         self.assertEqual(encoded_data_list.shape, (3,))
         npt.assert_array_equal(encoded_data_list, np.array([0, 0.5, 1]), "The encoded list is not correct")
 
+class TestStrClassificationIntEncoder(unittest.TestCase):
 
+    def setUp(self):
+        self.str_encoder = StrClassificationIntEncoder()
 
+    def test_encode(self):
+
+        # Test encoding a list of strings
+        encoded_data_list = self.str_encoder.encode_all(["A", "B", "C", "A"])
+        self.assertIsInstance(encoded_data_list, np.ndarray)
+        self.assertEqual(encoded_data_list.shape, (4,))
+        npt.assert_array_equal(encoded_data_list, np.array([0, 1, 2, 0]), "The encoded list is not correct")
 
 if __name__ == "__main__":
     unittest.main()
