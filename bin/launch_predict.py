74ad9177922c63c0ca9edf2045a14b9e3ab268a1
suzannejin <suzanne.jin.yao@gmail.com>
2024-07-18 16:40:39 +0200

    modify launch_predict.py to use polars instead of pandas

--- bin/launch_predict.py
+++ bin/launch_predict.py
@@ -3,9 +3,10 @@
 import argparse
 import json
 import os
-import pandas as pd
+import polars as pl
 import torch
 from torch.utils.data import DataLoader
+from typing import Tuple
 
 from src.data.handlertorch import TorchDataset
 from src.learner.predict import PredictWrapper
@@ -18,7 +19,7 @@ def get_args():
     parser = argparse.ArgumentParser(description="Predict the output of a model on a dataset.")
     parser.add_argument("-m", "--model", type=str, required=True, metavar="FILE", help='The model .py file')
     parser.add_argument("-w", "--weight", type=str, required=True, metavar="FILE", help="Model weights .pt file")
-    parser.add_argument("-mc", "--model_config", type=str, required=True, metavar="FILE", help="The tune config file.")
+    parser.add_argument("-mc", "--model_config", type=str, required=True, metavar="FILE", help="The tune config file with the model hyperparameters.")
     parser.add_argument("-ec", "--experiment_config", type=str, required=True, metavar="FILE", help='The experiment config used to modify the data.')
     parser.add_argument("-d", "--data", type=str, required=True, metavar="FILE", help='Input data')
     parser.add_argument("-o", "--output", type=str, required=True, metavar="FILE", help="output predictions csv file")
@@ -28,21 +29,33 @@ def get_args():
     args = parser.parse_args()
     return args
 
-def load_model(model_class: object, weight_path: str, mconfig_path: str) -> object:
+def load_model(model_class: object, weight_path: str, mconfig: dict) -> object:
     """
-    Load the model with its config and weights.
+    Load the model with its hyperparameters and weights.
     """
-    # load model config
-    with open(mconfig_path, 'r') as in_json:
-        mconfig = json.load(in_json)["model_params"]
-
-    # load model
-    model = model_class(**mconfig)
+    hyperparameters = mconfig["model_params"]
+    model = model_class(**hyperparameters)
     model.load_state_dict(torch.load(weight_path))
-
     return model
 
-def parse_y_keys(y: dict, data: pd.DataFrame, y_type='pred'):
+def get_batch_size(mconfig: dict) -> int:
+    """
+    Get the batch size from the model tune config dict.
+
+    If batch_size in model config, use it.
+    Otherwise use the default of 256.
+
+    TODO not sure how much this batch size is gonna 
+    affect the computation of forward prediction.
+    For the moment, just set to this.
+    """
+    batch_size = 256
+    if "data_params" in mconfig:
+        if "batch_size" in mconfig["data_params"]:
+            batch_size = mconfig["data_params"]["batch_size"]
+    return batch_size
+
+def parse_y_keys(y: dict, data: pl.DataFrame, y_type='pred'):
     """
     Parse the keys of the y dictionary.
 
@@ -67,9 +80,9 @@ def parse_y_keys(y: dict, data: pd.DataFrame, y_type='pred'):
 
     return parsed_y
 
-def add_meta_info(data: pd.DataFrame, df: pd.DataFrame):
+def add_meta_info(data: pl.DataFrame, y: dict):
     """
-    Add the meta columns to the data frame of predictions.
+    Add the meta columns to the dictionary of predictions and labels.
     In this way the output file can also contain the meta information.
     """
     # get meta keys
@@ -77,9 +90,9 @@ def add_meta_info(data: pd.DataFrame, df: pd.DataFrame):
 
     # add meta info
     for key in keys:
-        df[key] = data[key]
+        y[key] = data[key].to_list()
 
-    return df
+    return y
 
 def get_meta_keys(names: list):
     """
@@ -92,22 +105,27 @@ def get_meta_keys(names: list):
             keys.append(name)
     return keys
 
-def main(model_path: str, weight_path: str, mconfig_path: str, econfig_path: str, data_path: str, output: str, return_labels: bool = False):
+def main(model_path: str, weight_path: str, mconfig_path: str, econfig_path: str, data_path: str, output: str, return_labels: bool, split: Tuple[None, int]) -> None:
     
+    # load tune output config into dictionary
+    with open(mconfig_path, 'r') as in_json:
+        mconfig = json.load(in_json)
+
     # load model
     model_class = import_class_from_file(model_path)
-    model = load_model( model_class, weight_path, mconfig_path )
+    model = load_model( model_class, weight_path, mconfig )
 
     # read experiment config and retrieve experiment name and then initialize the experiment class
-    experiment_name = None
     with open(econfig_path, 'r') as in_json:
         d = json.load(in_json)
         experiment_name = d["experiment"]
     initialized_experiment_class = get_experiment(experiment_name)
 
-    # load and encode data
-    data = pd.read_csv(data_path)
-    dataloader = DataLoader(TorchDataset(data_path, initialized_experiment_class, split=split), batch_size=256, shuffle=False)  # TODO batch size is hard coded here, but maybe in the future we change it
+    # load and encode data into dataloder
+    dataloader = DataLoader(
+                    TorchDataset(data_path, initialized_experiment_class, split=split), 
+                    batch_size=get_batch_size(mconfig), 
+                    shuffle=False)
 
     # predict
     out = PredictWrapper(model, dataloader).predict(return_labels=return_labels)
@@ -115,19 +133,25 @@ def main(model_path: str, weight_path: str, mconfig_path: str, econfig_path: str
         y_pred, y_true = out
     else:
         y_pred, y_true = out, {}
+
+    # conver tensors to list
+    # otherwise polars cannot process this dictionary properly later when converting it into data frame
+    y_pred = {k: v.tolist() for k,v in y_pred.items()}
+    y_true = {k: v.tolist() for k,v in y_true.items()}
     
     # make the keys coherent with the columns from input data csv
+    data = pl.read_csv(data_path)
     y_pred = parse_y_keys(y_pred, data, y_type='pred')
     y_true = parse_y_keys(y_true, data, y_type='label')
 
-    # parse predictions (and labels) into a data frame
+    # parse predictions, labels and meta info into a data frame
     y = {**y_pred, **y_true}
-    df = pd.DataFrame(y)
-    df = add_meta_info(data, df)
+    y = add_meta_info(data, y)
+    df = pl.from_dict(y)
 
     # save output 
-    df.to_csv(output, index=False)
+    df.write_csv(output)
 
 if __name__ == "__main__":
     args = get_args()
-    main(args.model, args.weight, args.model_config, args.experiment_config, args.data, args.output, args.return_labels)
+    main(args.model, args.weight, args.model_config, args.experiment_config, args.data, args.output, args.return_labels, args.split)
